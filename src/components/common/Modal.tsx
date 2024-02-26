46729d56a8331e35440708b45fe1c34e77093a37
BaeDongWoo <ehddn5410@naver.com>
2024-02-26 21:38:09 +0900

    feat:커스텀 모달

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,132 @@
+import { Dispatch, useState } from 'react';
+import styled from 'styled-components';
+import { RoomHeaderprops } from '../waiting/room/RoomHeader';
+import { useNavigate } from 'react-router-dom';
+import { userInfo } from 'os';
+
+interface ModalProps extends RoomHeaderprops {
+  setIsModal: Dispatch<React.SetStateAction<boolean>>;
+}
+const Modal = ({ setIsModal, roomList, setRoomList }: ModalProps) => {
+  const nav = useNavigate();
+  const [isChecked, setIsChecked] = useState(false);
+  const [pwd, setPwd] = useState('');
+  const [title, setTitle] = useState('');
+  const addList = () => {
+    const roomInfo = {
+      title: title,
+      checked: isChecked,
+      pwd: pwd,
+      roomId: new Date().getTime(),
+    };
+    roomList.push(roomInfo);
+    setRoomList([...roomList]);
+    localStorage.setItem('roomList', JSON.stringify(roomList));
+    setIsModal(false);
+    nav(`/room/${roomInfo.roomId}`);
+  };
+
+  const closeModal = () => {
+    setIsModal(false);
+  };
+  return (
+    <Container>
+      <Title>
+        <h3>방 만들기</h3>
+      </Title>
+      <InputBox>
+        <input
+          placeholder="방 제목을 입력해 주세요"
+          onChange={(e: any) => setTitle(e.target.value)}
+        ></input>
+      </InputBox>
+      <PwdBox>
+        <div>
+          <input
+            type="password"
+            readOnly={!isChecked}
+            onChange={(e: any) => setPwd(e.target.value)}
+          ></input>
+          <input
+            type="checkbox"
+            onClick={(e: any) => setIsChecked(e.target.checked)}
+          ></input>
+          <span>비밀번호 설정</span>
+        </div>
+      </PwdBox>
+      <BtnBox>
+        <button id="close" onClick={closeModal}>
+          닫기
+        </button>
+        <button id="make" onClick={addList}>
+          만들기
+        </button>
+      </BtnBox>
+    </Container>
+  );
+};
+const Container = styled.div`
+  width: 500px;
+  height: 300px;
+  background-color: #e8f0fe;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  transform: translate(-50%, -50%);
+`;
+const Title = styled.div`
+  width: 100%;
+  height: 15%;
+  text-align: center;
+  background-color: #019696;
+  h3 {
+    line-height: 45px;
+    margin: 0;
+  }
+`;
+const InputBox = styled.div`
+  display: flex;
+  justify-content: center;
+  input {
+    width: 90%;
+    height: 30px;
+    border: 3px solid #00aaaa;
+    border-radius: 10px;
+  }
+`;
+const PwdBox = styled.div`
+  display: flex;
+  justify-content: center;
+  div {
+    width: 90%;
+    input[type='password'] {
+      width: 50%;
+      height: 30px;
+      border: 3px solid #00aaaa;
+      border-radius: 10px;
+    }
+    span {
+      color: #00aaaa;
+    }
+  }
+`;
+const BtnBox = styled.div`
+  height: 15%;
+  button {
+    border: none;
+    width: 50%;
+    height: 100%;
+  }
+  #close {
+    border-top: 1px solid #00aaaa;
+    background-color: #e8f0fe;
+    color: #00aaaa;
+    &:hover {
+      background-color: #d5e0f1;
+    }
+  }
+`;
+export default Modal;
