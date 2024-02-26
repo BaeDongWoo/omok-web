0156408a8711a32ab3d3716db722f6b2ae6b739a
BaeDongWoo <ehddn5410@naver.com>
2024-02-26 15:43:14 +0900

    feat:roomList props

--- src/pages/WaitingPage.tsx
+++ src/pages/WaitingPage.tsx
@@ -1,4 +1,33 @@
+import styled from 'styled-components';
+import Greeting from '../components/waiting/Greeting';
+import RoomHeader from '../components/waiting/room/RoomHeader';
+import RoomList from '../components/waiting/room/RoomList';
+import { useEffect, useState } from 'react';
+import { Roomtype } from '../components/waiting/room/Room';
+
 const WaitingPage = () => {
-  return <></>;
+  const [roomList, setRoomList] = useState<Roomtype[]>((): any => {
+    const list = sessionStorage.getItem('roomList') || '';
+    return list ? JSON.parse(list) : [];
+  });
+
+  useEffect(() => {
+    sessionStorage.setItem('roomList', JSON.stringify(roomList));
+    console.log('asdas');
+  }, [roomList]);
+  return (
+    <Container>
+      <Greeting />
+      <RoomHeader roomList={roomList} setRoomList={setRoomList} />
+      <b className="hr"></b>
+      <RoomList roomList={roomList} />
+    </Container>
+  );
 };
+const Container = styled.div`
+  width: 60%;
+  max-width: 1000px;
+  margin: auto;
+  padding: 100px;
+`;
 export default WaitingPage;
