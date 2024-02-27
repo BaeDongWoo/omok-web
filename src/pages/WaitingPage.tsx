ee4878cee494830b6e301c14f32b708c2fe0b9a9
BaeDongWoo <ehddn5410@naver.com>
2024-02-28 00:31:12 +0900

    feat:실시간 생성한  데이터 가져오기

--- src/pages/WaitingPage.tsx
+++ src/pages/WaitingPage.tsx
@@ -4,16 +4,35 @@ import RoomHeader from '../components/waiting/room/RoomHeader';
 import RoomList from '../components/waiting/room/RoomList';
 import { useEffect, useState } from 'react';
 import { Roomtype } from '../components/waiting/room/Room';
+import { fireStore } from '../config/firebaseConfig';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 const WaitingPage = () => {
-  const [roomList, setRoomList] = useState<Roomtype[]>((): any => {
-    const list = localStorage.getItem('roomList') || '';
-    return list ? JSON.parse(list) : [];
-  });
+  const [roomList, setRoomList] = useState<Roomtype[]>([]);
 
   useEffect(() => {
-    localStorage.setItem('roomList', JSON.stringify(roomList));
-  }, [roomList]);
+    const unsubscribe = onSnapshot(
+      collection(fireStore, 'rooms'),
+      (snapshot) => {
+        const updatedRooms: Roomtype[] = [];
+        snapshot.forEach((room) => {
+          const roomInfo = {
+            title: room.data().roomTitle,
+            roomId: room.data().roomId,
+            checked: room.data().checked,
+            pwd: room.data().pwd,
+            users: room.data().users.length,
+          };
+          updatedRooms.push(roomInfo);
+        });
+        setRoomList(updatedRooms);
+      }
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   return (
     <Container>
       <Greeting />
