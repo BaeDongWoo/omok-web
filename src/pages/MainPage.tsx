1faea91875114f7c42b1faab9c60430b11cb1b92
BaeDongWoo <ehddn5410@naver.com>
2024-02-28 03:06:26 +0900

    feat:유저 입장 및 퇴장 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,51 @@
+import {
+  DocumentData,
+  doc,
+  getDoc,
+  onSnapshot,
+  updateDoc,
+} from 'firebase/firestore';
+import { useEffect } from 'react';
+import { fireStore } from '../config/firebaseConfig';
+import { useParams } from 'react-router-dom';
+import { Roomtype } from '../components/waiting/room/Room';
+
 const MainPage = () => {
+  const { roomId } = useParams();
+  const uid = localStorage.getItem('uid');
+  useEffect(() => {
+    try {
+      if (roomId) {
+        const getData = async () => {
+          const data: DocumentData = await getDoc(
+            doc(fireStore, 'rooms', roomId)
+          );
+          let users = data.data().users;
+          users.push(uid);
+          updateDoc(doc(fireStore, 'rooms', roomId), { users: users });
+        };
+        getData();
+        const unsubscribe = onSnapshot(
+          doc(fireStore, 'rooms', roomId),
+          (snapshot) => {}
+        );
+        return () => {
+          const getData = async () => {
+            const data: DocumentData = await getDoc(
+              doc(fireStore, 'rooms', roomId)
+            );
+            let users = data.data().users;
+            users = users.filter((user: string) => user !== uid);
+            updateDoc(doc(fireStore, 'rooms', roomId), { users: users });
+          };
+          getData();
+          unsubscribe();
+        };
+      } else throw new Error();
+    } catch (e) {
+      console.error(e);
+    }
+  }, []);
   return <>메인페이지</>;
 };
 export default MainPage;
