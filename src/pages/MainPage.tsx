08061c474ae62503d6b997febfaf893b5781c37e
BaeDongWoo <ehddn5410@naver.com>
2024-02-28 08:50:00 +0900

    feat:비동기로 데이터를 처리하기위한 함수 추가:

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -8,29 +8,40 @@ import {
 } from 'firebase/firestore';
 import { useEffect } from 'react';
 import { fireStore } from '../config/firebaseConfig';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import Board from '../components/main/board/Board';
 
 const MainPage = () => {
+  const nav = useNavigate();
   const { roomId } = useParams();
   const uid = localStorage.getItem('uid');
   useEffect(() => {
     try {
       if (roomId) {
+        let unsubscribe: any;
         const getData = async () => {
           const data: DocumentData = await getDoc(
             doc(fireStore, 'rooms', roomId)
           );
           let users = data.data().users;
           users.push(uid);
+          users = Array.from(new Set(users));
           await updateDoc(doc(fireStore, 'rooms', roomId), { users: users });
         };
-        getData();
-        const unsubscribe = onSnapshot(
-          doc(fireStore, 'rooms', roomId),
-          (snapshot) => {}
-        );
+        const startSnapshot = () => {
+          unsubscribe = onSnapshot(
+            doc(fireStore, 'rooms', roomId),
+            (snapshot) => {}
+          );
+        };
+        const snapshot = async () => {
+          //비동기로 데이터를 처리하기 위함
+          await getData();
+          startSnapshot();
+        };
+        snapshot();
         return () => {
-          const getData = async () => {
+          const removeData = async () => {
             const data: DocumentData = await getDoc(
               doc(fireStore, 'rooms', roomId)
             );
@@ -38,13 +49,15 @@ const MainPage = () => {
             users = users.filter((user: string) => user !== uid);
             if (users.length === 0) {
               await deleteDoc(doc(fireStore, 'rooms', roomId));
+              // 뒤로가기시 랜더링 될때 해당 방이 나왔다가 사라짐
+              // 후에 redux를 통해 roomList를 업데이트처리 예정
             } else {
               await updateDoc(doc(fireStore, 'rooms', roomId), {
                 users: users,
               });
             }
           };
-          getData();
+          removeData();
           unsubscribe();
         };
       } else throw new Error();
@@ -52,6 +65,11 @@ const MainPage = () => {
       console.error(e);
     }
   }, []);
-  return <>메인페이지</>;
+  return (
+    <>
+      <button onClick={() => nav('/waiting')}>벋흔</button>
+      <Board />
+    </>
+  );
 };
 export default MainPage;
