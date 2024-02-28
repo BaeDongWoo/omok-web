260f9184c1437a13d077892dec1b59dcc78cfb81
BaeDongWoo <ehddn5410@naver.com>
2024-02-28 11:50:00 +0900

    feat:보드판 위에서 움직이는 마우스의 좌표값 가져오는 함수 추가

--- src/components/main/board/Board.tsx
+++ src/components/main/board/Board.tsx
@@ -1,8 +1,4 @@
-// const Board = () => {
-//   return <></>;
-// };
-// export default Board;
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 interface Cell {
@@ -12,7 +8,8 @@ interface Cell {
 
 const Board = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const boardSize = 18;
+  const [stones, setStones] = useState<Cell[]>([]);
+  const boardSize = 19;
   const cellSize = 20;
   const cavasSize = boardSize * cellSize;
 
@@ -21,8 +18,9 @@ const Board = () => {
     const ctx = canvas?.getContext('2d');
     if (ctx) {
       drawBoard(ctx);
+      console.log(stones);
     }
-  }, []);
+  }, [stones]);
 
   const drawBoard = (ctx: CanvasRenderingContext2D) => {
     ctx.strokeStyle = '#00aaaa';
@@ -38,6 +36,14 @@ const Board = () => {
     }
     ctx.closePath();
   };
+  const onMouseMove = (e: any) => {
+    const canvas = canvasRef.current;
+    let index = canvas?.getBoundingClientRect();
+    if (index) {
+      const [x, y] = [e.clientX - index.left, e.clientY - index.top];
+      console.log(x, y);
+    }
+  };
   return (
     <Container>
       <BoardGrid
@@ -45,6 +51,7 @@ const Board = () => {
         width={cavasSize}
         height={cavasSize}
         style={{ border: '3px solid #00aaaa' }}
+        onMouseMove={onMouseMove}
       />
     </Container>
   );
