578a1005e769a606d4afb8c23fbe79a9e7d9ee32
BaeDongWoo <ehddn5410@naver.com>
2024-02-29 23:54:04 +0900

    feat:사용자 닉네임 표시, 본인 오른쪽,상대 왼쪽

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -10,18 +10,16 @@ import { useEffect, useState } from 'react';
 import { fireStore } from '../config/firebaseConfig';
 import { useNavigate, useParams } from 'react-router-dom';
 import Board from '../components/main/board/Board';
-export interface roomCheck {
-  userCnt: number;
-  stoneNum: number;
-}
+import styled from 'styled-components';
+
 const MainPage = () => {
   const nav = useNavigate();
-  const [roomUsersCnt, setRoomUserCnt] = useState<roomCheck>({
-    userCnt: 0,
-    stoneNum: 0,
-  });
-  const { roomId } = useParams();
   const uid = localStorage.getItem('uid');
+  const nickname = localStorage.getItem('nickname');
+  const { roomId, roomTitle } = useParams();
+  const [me, setMe] = useState(nickname);
+  const [user, setUser] = useState('user');
+
   useEffect(() => {
     try {
       if (roomId) {
@@ -33,21 +31,32 @@ const MainPage = () => {
           let users = data.data().users;
           users.push(uid);
           users = Array.from(new Set(users));
-          await updateDoc(doc(fireStore, 'rooms', roomId), { users: users });
+          let nick = data.data().nickname;
+          nick.push(nickname);
+          await updateDoc(doc(fireStore, 'rooms', roomId), {
+            users: users,
+            nickname: nick,
+          });
+          if (users.length === 2) {
+            await updateDoc(doc(fireStore, 'rooms', roomId), {
+              game: { startGame: true, turn: 0 },
+            });
+          }
         };
-        const startSnapshot = () => {
+        const startSnapshot = async () => {
           unsubscribe = onSnapshot(
             doc(fireStore, 'rooms', roomId),
             (snapshot) => {
               const data = snapshot?.data();
               if (data) {
-                const stoneNum = data.users.findIndex(
-                  (id: string) => id === uid
+                const getNick = data.nickname.filter(
+                  (nick: string) => nick !== nickname
                 );
-                setRoomUserCnt({
-                  userCnt: data.users.length,
-                  stoneNum: stoneNum,
-                });
+                if (getNick.length === 0) {
+                  setUser('user');
+                } else {
+                  setUser(getNick[0]);
+                }
               }
             }
           );
@@ -65,6 +74,8 @@ const MainPage = () => {
             );
             let users = data.data().users;
             users = users.filter((user: string) => user !== uid);
+            let nick = data.data().nickname;
+            nick = nick.filter((ni: string) => ni !== nickname);
             if (users.length === 0) {
               await deleteDoc(doc(fireStore, 'rooms', roomId));
               // 뒤로가기시 랜더링 될때 해당 방이 나왔다가 사라짐
@@ -72,11 +83,13 @@ const MainPage = () => {
             } else {
               await updateDoc(doc(fireStore, 'rooms', roomId), {
                 users: users,
+                nickname: nick,
+                game: { startGame: false, turn: 0 },
               });
             }
+            unsubscribe();
           };
           removeData();
-          unsubscribe();
         };
       } else throw new Error();
     } catch (e) {
@@ -85,9 +98,23 @@ const MainPage = () => {
   }, []);
   return (
     <>
-      <button onClick={() => nav('/waiting')}>벋흔</button>
-      <Board userProps={roomUsersCnt} />
+      <TitleBox>{roomTitle}</TitleBox>
+      <GameBox>
+        <div>{user}</div>
+        <Board />
+        <div>{me}</div>
+      </GameBox>
     </>
   );
 };
+const TitleBox = styled.h1`
+  color: #e8f0fe;
+  text-align: center;
+  margin-top: 100px;
+  margin-bottom: 50px;
+`;
+const GameBox = styled.div`
+  display: flex;
+  color: #e8f0fe;
+`;
 export default MainPage;
